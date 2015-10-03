65d75d730e6912b4918d918f25dbf194b78ed6a4
Hamed Nourhani <info@itstar.ir>
2015-10-03 08:58:51 +0330

    flags added

--- library/FrontEndTutorialPart1/footer.php
+++
@@ -1,9 +0,0 @@
-	</div><!-- .container END -->
-
-	<!-- Theme Hook -->
-	<?php wp_footer(); ?>
-			
-<!--END body-->
-</body>
-<!--END html-->
-</html>
--- library/FrontEndTutorialPart3/sidebar.php
+++
@@ -1,4 +0,0 @@
-		<!-- #sidebar BEGIN -->
-        <div id="sidebar">
-            <?php /* Widgetised Area */ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar() ) ?>
-        </div><!-- #sidebar END -->
